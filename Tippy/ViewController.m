b53b40419460c2e4ef6345b1537e8ec8f7d46f89
Kaitlyn Gunadhi <kgunadhi@fb.com>
2020-06-23 20:09:41 -0700

    Built tip calculator and improved UI

--- Tippy/ViewController.m
+++ Tippy/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
 @end
 
 @implementation ViewController
@@ -19,5 +24,36 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Hello");
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    double bill = [self.billField.text doubleValue];
+    
+    NSArray *percentages = @[@(0.15), @(0.18), @(0.2)];
+    
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
+
+- (IBAction)onEditingBegin:(id)sender {
+    [UIView animateWithDuration:0.2 animations:^{
+        self.tipLabel.alpha = 0.3;
+        self.totalLabel.alpha = 0.3;
+    }];
+}
+
+- (IBAction)onEditingEnd:(id)sender {
+    [UIView animateWithDuration:0.2 animations:^{
+        self.tipLabel.alpha = 1;
+        self.totalLabel.alpha = 1;
+    }];
+}
 
 @end
