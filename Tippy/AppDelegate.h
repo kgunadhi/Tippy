3a9827cf8b4232b77796f81d3cf14c90f79767de
Kaitlyn Gunadhi <kgunadhi@fb.com>
2020-06-23 18:03:46 -0700

    Initial Commit

---
+++ Tippy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tippy
+//
+//  Created by Kaitlyn Gunadhi on 6/23/20.
+//  Copyright © 2020 Kaitlyn Gunadhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tippy/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Tippy
+//
+//  Created by Kaitlyn Gunadhi on 6/23/20.
+//  Copyright © 2020 Kaitlyn Gunadhi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
